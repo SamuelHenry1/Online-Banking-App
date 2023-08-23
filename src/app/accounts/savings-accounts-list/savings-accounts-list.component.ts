08d81a8ed2a9534ffc96aa1fda81c5aecd601fa3
Samuel Henry <samuel.henry@capgemini.com>
2023-08-23 17:48:09 -0400

    Add test tab

--- src/app/accounts/savings-accounts-list/savings-accounts-list.component.ts
+++ src/app/accounts/savings-accounts-list/savings-accounts-list.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
-import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
-import { MatLegacyPaginator as MatPaginator } from '@angular/material/legacy-paginator';
-import { MatSort } from '@angular/material/sort';
-import { startWith, delay, tap } from 'rxjs/operators';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  AfterViewInit,
+} from "@angular/core";
+import { MatLegacyTableDataSource as MatTableDataSource } from "@angular/material/legacy-table";
+import { MatLegacyPaginator as MatPaginator } from "@angular/material/legacy-paginator";
+import { MatSort } from "@angular/material/sort";
+import { startWith, delay, tap } from "rxjs/operators";
 
 @Component({
-  selector: 'online-banking-savings-accounts-list',
-  templateUrl: './savings-accounts-list.component.html',
-  styleUrls: ['./savings-accounts-list.component.css']
+  selector: "online-banking-savings-accounts-list",
+  templateUrl: "./savings-accounts-list.component.html",
+  styleUrls: ["./savings-accounts-list.component.css"],
 })
 export class SavingsAccountsListComponent implements OnInit, AfterViewInit {
-
   @Input() savingsAccounts: any;
 
-  displayedColumns: string[] = ['accountNo', 'status', 'type' , 'accountBalance', 'submittedOn' ];
+  displayedColumns: string[] = [
+    "accountNo",
+    "status",
+    "type",
+    "accountBalance",
+    "submittedOn",
+  ];
 
   dataSource: MatTableDataSource<any>;
 
@@ -21,21 +32,19 @@ export class SavingsAccountsListComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     /* Refer: https://blog.angular-university.io/angular-debugging/  workaround with delayed data binding */
     this.paginator.page
-          .pipe(
-            startWith(null),
-            delay(0),
-            tap(() => this.setAccountsTable()
-            )
-          ).subscribe();
+      .pipe(
+        startWith(null),
+        delay(0),
+        tap(() => this.setAccountsTable())
+      )
+      .subscribe();
   }
 
   /**
@@ -53,5 +62,4 @@ export class SavingsAccountsListComponent implements OnInit, AfterViewInit {
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
 }
